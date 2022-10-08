78a7c152ba6511fbf9bd2c5478193f4c951c90fb
Cheth <chethiya.w@gmail.com>
2022-10-08 20:11:19 +1100

    Added report class and tests.

---
+++ ToyRobot/Report.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ToyRobot.Enums;
+using ToyRobot.Interfaces;
+
+namespace ToyRobot
+{
+    public class Report : IReport
+    {
+        public void Print(int x, int y, Face face)
+        {
+            Console.WriteLine("Output : {0} {1} {2}", x, y, face);
+        }
+    }
+}
---
+++ ToyRobotTests/ReportTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using ToyRobot;
+using ToyRobot.Enums;
+using ToyRobot.Interfaces;
+using Xunit;
+
+namespace ToyRobotTests
+{
+    public class ReportTests
+    {
+        [Fact]
+        public void Print()
+        {
+            IReport report = new Report();
+
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            report.Print(2, 2, Face.NORTH);
+
+            var outputString = output.ToString();
+
+            Assert.Equal("Output : 2 2 NORTH\r\n", outputString);
+        }
+    }
+}
