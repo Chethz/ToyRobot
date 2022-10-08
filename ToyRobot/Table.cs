4b2bc289304811af1c6f50b5b8939d937c5e2b4c
Cheth <chethiya.w@gmail.com>
2022-10-08 20:28:53 +1100

    Added table class and tests.

---
+++ ToyRobot/Table.cs
@@ -0,0 +1,24 @@
+﻿using ToyRobot.Interfaces;
+
+namespace ToyRobot
+{
+    public class Table : ITable
+    {
+        private int _length;
+        private int _width;
+
+        public int Length { get => _length; }
+        public int Width { get => _width; }
+
+        public Table(int length, int width)
+        {
+            _length = length;
+            _width = width;
+        }
+
+        public bool IsValidPlace(int x, int y)
+        {
+            return (x >= 0 && x <= Width && y >= 0 && y <= Length);
+        }
+    }
+}
---
+++ ToyRobotTests/TableTests.cs
@@ -0,0 +1,63 @@
+﻿
+using ToyRobot;
+using Xunit;
+
+namespace ToyRobotTests
+{
+    public class TableTests
+    {
+        [Fact]
+        public void IsValidPlace_WhenValidPlace_ReturnTrue()
+        {
+            Table table = new Table(5, 5);
+            var result = table.IsValidPlace(3, 3);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsValidPlace_WhenXIsInValidPlace_ReturnFalse()
+        {
+            Table table = new Table(5, 5);
+            var result = table.IsValidPlace(6, 3);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValidPlace_WhenYIsInValidPlace_ReturnFalse()
+        {
+            Table table = new Table(5, 5);
+            var result = table.IsValidPlace(3, 6);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValidPlace_WhenBothAreInValidPlaces_ReturnFalse()
+        {
+            Table table = new Table(5, 5);
+            var result = table.IsValidPlace(6, 6);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValidPlace_WhenValuesAreLessThanZero_ReturnFalse()
+        {
+            Table table = new Table(5, 5);
+            var result = table.IsValidPlace(-1, -6);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValidPlace_WhenValuesAreZero_ReturnTrue()
+        {
+            Table table = new Table(5, 5);
+            var result = table.IsValidPlace(0, 0);
+
+            Assert.True(result);
+        }
+    }
+}
