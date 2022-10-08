06ef62754e5302fabe401fd7862dd54c3719b2ca
Cheth <chethiya.w@gmail.com>
2022-10-08 21:00:40 +1100

    Added simulator class and tests.

---
+++ ToyRobotSimulator/Simulator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using ToyRobot.Interfaces;
+
+namespace ToyRobotSimulator
+{
+    public class Simulator
+    {
+        private ITable _table;
+        private IRobot _robot;
+
+        public Simulator(ITable table, IRobot robot)
+        {
+            _table = table;
+            _robot = robot;
+        }
+
+
+        public void ProcessCommand(string input)
+        {
+            string commands = input.Trim().ToUpper();
+
+            Regex place = new Regex(@"^PLACE ");
+            Regex move = new Regex(@"^MOVE$");
+            Regex left = new Regex(@"^LEFT$");
+            Regex right = new Regex(@"^RIGHT$");
+            Regex report = new Regex(@"^REPORT$");
+
+
+            if (place.IsMatch(commands))
+            {
+                ParsePlaceCommand(commands);
+            }
+            else if (move.IsMatch(commands))
+            {
+                _robot.Move();
+            }
+            else if (left.IsMatch(commands))
+            {
+                _robot.TurnRobot(commands);
+            }
+            else if (right.IsMatch(commands))
+            {
+                _robot.TurnRobot(commands);
+            }
+            else if (report.IsMatch(commands))
+            {
+                _robot.Report();
+            }
+            else
+            {
+                throw new Exception("Command is not valid : " + input);
+            }
+        }
+
+
+        private void ParsePlaceCommand(string command)
+        {
+            string[] coordinates = command.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (coordinates.Length == 4)
+            {
+                int x;
+                int y;
+
+                if (int.TryParse(coordinates[1], out x) && int.TryParse(coordinates[2], out y) && coordinates[3] != null)
+                {
+                    _robot.PlaceRobot(x, y, coordinates[3]);
+                }
+                else
+                {
+                    throw new Exception("Invalid place command.");
+                }
+            }
+            else
+            {
+                throw new Exception("Invalid place command.");
+            }
+
+        }
+    }
+}
---
+++ ToyRobotSimulatorTests/ToyRobotSimulatorTests.cs
@@ -0,0 +1,160 @@
+﻿using ToyRobotSimulator;
+using ToyRobot;
+using ToyRobot.Interfaces;
+using Xunit;
+using System;
+
+namespace ToyRobotSimulatorTests
+{
+    public class ToyRobotSimulatorTests
+    {
+        [Fact]
+        public void ProcessCommand_WhenPassedInValidCommand_Throwexception()
+        {
+            ITable table = new Table(5, 5);
+            IMove move = new Move();
+            IReport report = new Report();
+            IRobot robot = new Robot(table, move, report);
+
+            var Simulator = new Simulator(table, robot);
+
+            Action result = () => Simulator.ProcessCommand("RUN");
+            Exception exception = Assert.Throws<Exception>(result);
+
+            Assert.Equal("Command is not valid : RUN", exception.Message);
+        }
+
+        [Fact]
+        public void ProcessCommand_WhenPassedInValidPlaceCommand_Returnexception()
+        {
+            ITable table = new Table(5, 5);
+            IMove move = new Move();
+            IReport report = new Report();
+            IRobot robot = new Robot(table, move, report);
+
+            var Simulator = new Simulator(table, robot);
+
+            Action result = () => Simulator.ProcessCommand("PLACE A, B, NORTH");
+
+            Exception exception = Assert.Throws<Exception>(result);
+
+            Assert.Equal("Invalid place command.", exception.Message);
+        }
+
+
+        [Fact]
+        public void ProcessCommand_WhenPlaceRobot_PlacedOnTable()
+        {
+            ITable table = new Table(5, 5);
+            IMove move = new Move();
+            IReport report = new Report();
+            IRobot robot = new Robot(table, move, report);
+
+            var Simulator = new Simulator(table, robot);
+
+            Simulator.ProcessCommand("PLACE 2, 2, NORTH");
+
+            Assert.Equal(2, robot.XPosition);
+        }
+
+        [Fact]
+        public void ProcessCommand_WhenMove_MoveRobot()
+        {
+            ITable table = new Table(5, 5);
+            IMove move = new Move();
+            IReport report = new Report();
+            IRobot robot = new Robot(table, move, report);
+
+            var Simulator = new Simulator(table, robot);
+
+            Simulator.ProcessCommand("PLACE 2, 2, NORTH");
+            Simulator.ProcessCommand("MOVE");
+            Assert.Equal(3, robot.YPosition);
+        }
+
+        [Fact]
+        public void ProcessCommand_WhenInvalidMove_ThrowException()
+        {
+            ITable table = new Table(5, 5);
+            IMove move = new Move();
+            IReport report = new Report();
+            IRobot robot = new Robot(table, move, report);
+
+            var Simulator = new Simulator(table, robot);
+
+            Simulator.ProcessCommand("PLACE 2, 2, NORTH");
+            Action result = () => Simulator.ProcessCommand("MO0VE");
+            Exception exception = Assert.Throws<Exception>(result);
+
+            Assert.Equal("Command is not valid : MO0VE", exception.Message);
+        }
+
+        [Fact]
+        public void ProcessCommand_WhenMoveWithSpaceAtFront_MoveRobot()
+        {
+            ITable table = new Table(5, 5);
+            IMove move = new Move();
+            IReport report = new Report();
+            IRobot robot = new Robot(table, move, report);
+
+            var Simulator = new Simulator(table, robot);
+
+            Simulator.ProcessCommand("PLACE 2, 2, NORTH");
+            Simulator.ProcessCommand(" MOVE");
+
+            Assert.Equal(3, robot.YPosition);
+        }
+
+        [Fact]
+        public void ProcessCommand_WhenTurnLeft_TurnRobot()
+        {
+            ITable table = new Table(5, 5);
+            IMove move = new Move();
+            IReport report = new Report();
+            IRobot robot = new Robot(table, move, report);
+
+            var Simulator = new Simulator(table, robot);
+
+            Simulator.ProcessCommand("PLACE 2, 2, NORTH");
+            Simulator.ProcessCommand("LEFT");
+
+            Assert.Equal(Face.WEST, robot.Face);
+        }
+
+        [Fact]
+        public void ProcessCommand_WhenTurnRight_TurnRobot()
+        {
+            ITable table = new Table(5, 5);
+            IMove move = new Move();
+            IReport report = new Report();
+            IRobot robot = new Robot(table, move, report);
+
+            var Simulator = new Simulator(table, robot);
+
+            Simulator.ProcessCommand("PLACE 2, 2, NORTH");
+            Simulator.ProcessCommand("RIGHT");
+
+            Assert.Equal(Face.EAST, robot.Face);
+        }
+
+        [Fact]
+        public void ProcessCommand_WhenReport_PrintReport()
+        {
+            ITable table = new Table(5, 5);
+            IMove move = new Move();
+            IReport report = new Report();
+            IRobot robot = new Robot(table, move, report);
+
+            var Simulator = new Simulator(table, robot);
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            Simulator.ProcessCommand("PLACE 2, 2, NORTH");
+            Simulator.ProcessCommand("REPORT");
+
+            var outputString = output.ToString();
+
+            Assert.Equal("Output : 2 2 NORTH\r\n", outputString);
+        }
+    }
+}
