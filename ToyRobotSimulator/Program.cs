dc2510ec5c443b6571d3a7f932a413b1542b28a1
Cheth <chethiya.w@gmail.com>
2022-10-08 21:30:59 +1100

    Added console program.

---
+++ ToyRobotSimulator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ToyRobot;
+using ToyRobot.Interfaces;
+
+namespace ToyRobotSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ITable table = new Table(5, 5);
+            IMove move = new Move();
+            IReport report = new Report();
+            IRobot robot = new Robot(table, move, report);
+
+            Simulator simulator = new Simulator(table, robot);
+
+            var stopApplication = false;
+            Console.WriteLine("Toy Robot Simulator Started");
+            Console.WriteLine("Please place robot to start simulator");
+            do
+            {
+                var command = Console.ReadLine();
+                if (command == null) continue;
+
+                if (command.Equals("EXIT"))
+                    stopApplication = true;
+                else
+                {
+                    try
+                    {
+                        simulator.ProcessCommand(command);
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+            } while (!stopApplication);
+        }
+    }
+}
