e4412206247d9795d657e285e539f9dc1faf3233
Cheth <chethiya.w@gmail.com>
2022-10-08 20:40:07 +1100

    Added robot class and tests.

---
+++ ToyRobot/Robot.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using ToyRobot.Enums;
+using ToyRobot.Interfaces;
+
+namespace ToyRobot
+{
+    public class Robot : IRobot
+    {
+        private IMove _move;
+        private ITable _table;
+        private IReport _report;
+        private int _xPosition;
+        private int _yPosition;
+        private Face _face;
+        private bool IsPlaced = false;
+
+        int IRobot.XPosition { get => _xPosition; set => _xPosition = value; }
+        int IRobot.YPosition { get => _yPosition; set => _yPosition = value; }
+        Face IRobot.Face { get => _face; set => _face = value; }
+
+        public Robot(ITable table, IMove move, IReport report)
+        {
+            _move = move;
+            _table = table;
+            _report = report;
+        }
+
+        public void Move()
+        {
+            if (!IsPlaced)
+            {
+                return;
+            }
+
+            _move.NextLocation(_xPosition, _yPosition, _face);
+            if (_table.IsValidPlace(_move.NextX, _move.NextY))
+            {
+                _xPosition = _move.NextX;
+                _yPosition = _move.NextY;
+            }
+            else
+            {
+                throw new Exception("Invalid move");
+            }
+        }
+
+        public void PlaceRobot(int x, int y, string face)
+        {
+            if (_table == null)
+            {
+                throw new Exception("Please add table.");
+            }
+
+            if (!_table.IsValidPlace(x, y))
+            {
+                throw new Exception("Invalid position");
+            }
+
+            _xPosition = x;
+            _yPosition = y;
+            _face = GetFace(face);
+            IsPlaced = true;
+        }
+
+        public void Report()
+        {
+            if (IsPlaced)
+            {
+                _report.Print(_xPosition, _yPosition, _face);
+            }
+        }
+
+        public void TurnRobot(string command)
+        {
+            if (IsPlaced)
+            {
+                Side turnCommand = GetTurnCommand(command);
+
+                if (turnCommand == Side.LEFT)
+                {
+                    _face = TurnLeft(_face);
+                }
+                else
+                {
+                    _face = TurnRight(_face);
+                }
+            }
+            else
+            {
+                throw new Exception("Robot is not placed on the table");
+            }
+        }
+
+        private Face TurnLeft(Face face)
+        {
+            var newFace = face;
+            switch (face)
+            {
+                case Face.NORTH:
+                    newFace = Face.WEST;
+                    break;
+                case Face.SOUTH:
+                    newFace = Face.EAST;
+                    break;
+                case Face.EAST:
+                    newFace = Face.NORTH;
+                    break;
+                case Face.WEST:
+                    newFace = Face.SOUTH;
+                    break;
+            }
+            return newFace;
+        }
+
+        private Face TurnRight(Face face)
+        {
+            var newFace = face;
+            switch (face)
+            {
+                case Face.NORTH:
+                    newFace = Face.EAST;
+                    break;
+                case Face.SOUTH:
+                    newFace = Face.WEST;
+                    break;
+                case Face.EAST:
+                    newFace = Face.SOUTH;
+                    break;
+                case Face.WEST:
+                    newFace = Face.NORTH;
+                    break;
+            }
+            return newFace;
+        }
+
+        private Face GetFace(string inputFace)
+        {
+            if (string.IsNullOrEmpty(inputFace))
+            {
+                throw new Exception("Empty face command");
+            }
+
+            inputFace = inputFace.Trim().ToUpper();
+
+            switch (inputFace)
+            {
+                case "NORTH":
+                    return Face.NORTH;
+                case "SOUTH":
+                    return Face.SOUTH;
+                case "EAST":
+                    return Face.EAST;
+                case "WEST":
+                    return Face.WEST;
+                default:
+                    throw new Exception("Invalid face");
+            }
+        }
+
+        private Side GetTurnCommand(string command)
+        {
+            if (string.IsNullOrEmpty(command))
+            {
+                throw new Exception("Empty command");
+            }
+
+            command = command.Trim().ToUpper();
+
+            switch (command)
+            {
+                case "LEFT":
+                    return Side.LEFT;
+                case "RIGHT":
+                    return Side.RIGHT;
+                default:
+                    throw new Exception("Invalid command : " + command);
+            }
+        }
+    }
+}
---
+++ ToyRobotTests/RobotTests.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using ToyRobot;
+using ToyRobot.Enums;
+using ToyRobot.Interfaces;
+using Xunit;
+
+namespace ToyRobotTests
+{
+    public class RobotTests
+    {
+        [Fact]
+        public void PlaceRobot_XPositionIsTwo()
+        {
+            ITable table = new Table(5, 5);
+            IReport report = new Report();
+            IMove move = new Move();
+            IRobot robot = new Robot(table, move, report);
+
+            robot.PlaceRobot(2, 2, "NORTH");
+
+            Assert.Equal(2, robot.XPosition);
+        }
+
+        [Fact]
+        public void PlaceRobot_YPositionIsTwo()
+        {
+            ITable table = new Table(5, 5);
+            IReport report = new Report();
+            IMove move = new Move();
+            IRobot robot = new Robot(table, move, report);
+
+            robot.PlaceRobot(2, 2, "NORTH");
+
+            Assert.Equal(2, robot.YPosition);
+        }
+
+        [Fact]
+        public void PlaceRobot_FaceIsNorth()
+        {
+            ITable table = new Table(5, 5);
+            IReport report = new Report();
+            IMove move = new Move();
+            IRobot robot = new Robot(table, move, report);
+
+            robot.PlaceRobot(2, 2, "NORTH");
+
+            Assert.Equal(Face.NORTH, robot.Face);
+        }
+
+        [Fact]
+        public void PlaceRobot_WhenTableIsNull_ReturnException()
+        {
+            IReport report = new Report();
+            IMove move = new Move();
+            IRobot robot = new Robot(null, move, report);
+
+            Action result = () => robot.PlaceRobot(2, 2, "NORTH");
+
+            Exception exception = Assert.Throws<Exception>(result);
+
+            Assert.Equal("Please add table.", exception.Message);
+        }
+
+        [Fact]
+        public void PlaceRobot_WhenInValidPosition_ReturnException()
+        {
+            ITable table = new Table(5, 5);
+            IReport report = new Report();
+            IMove move = new Move();
+            IRobot robot = new Robot(table, move, report);
+
+            Action result = () => robot.PlaceRobot(6, 6, "NORTH");
+
+            Exception exception = Assert.Throws<Exception>(result);
+
+            Assert.Equal("Invalid position", exception.Message);
+        }
+
+        [Fact]
+        public void PlaceRobot_WhenInValidFace_ReturnException()
+        {
+            ITable table = new Table(5, 5);
+            IReport report = new Report();
+            IMove move = new Move();
+            IRobot robot = new Robot(table, move, report);
+
+            Action result = () => robot.PlaceRobot(3, 3, "");
+
+            Exception exception = Assert.Throws<Exception>(result);
+
+            Assert.Equal("Empty face command", exception.Message);
+        }
+
+        [Fact]
+        public void TurnRobot_WhenTurnLeftWhileFaceNorth_ReturnWest()
+        {
+            ITable table = new Table(5, 5);
+            IReport report = new Report();
+            IMove move = new Move();
+            IRobot robot = new Robot(table, move, report);
+
+            robot.PlaceRobot(3, 3, "NORTH");
+            robot.TurnRobot("LEFT");
+
+            Assert.Equal(Face.WEST, robot.Face);
+        }
+
+        [Fact]
+        public void TurnRobot_WhenTurnRightWhileFaceNorth_ReturnEast()
+        {
+            ITable table = new Table(5, 5);
+            IReport report = new Report();
+            IMove move = new Move();
+            IRobot robot = new Robot(table, move, report);
+
+            robot.PlaceRobot(3, 3, "NORTH");
+            robot.TurnRobot("RIGHT");
+
+            Assert.Equal(Face.EAST, robot.Face);
+        }
+
+        [Fact]
+        public void TurnRobot_WhenRobotIsNotPlaced_ReturnException()
+        {
+            IReport report = new Report();
+            IMove move = new Move();
+            IRobot robot = new Robot(null, move, report);
+
+            Action result = () => robot.TurnRobot("RIGHT");
+            Exception exception = Assert.Throws<Exception>(result);
+
+            Assert.Equal("Robot is not placed on the table", exception.Message);
+        }
+
+        [Fact]
+        public void TurnRobot_WhenInvaliTurnCommandGiven_ReturnException()
+        {
+            ITable table = new Table(5, 5);
+            IReport report = new Report();
+            IMove move = new Move();
+            IRobot robot = new Robot(table, move, report);
+
+            robot.PlaceRobot(3, 3, "NORTH");
+            Action result = () => robot.TurnRobot("RIGGHT");
+            Exception exception = Assert.Throws<Exception>(result);
+
+            Assert.Equal("Invalid command : RIGGHT", exception.Message);
+        }
+
+        [Fact]
+        public void TurnRobot_WhenNullOrEmptyCommandGiven_ReturnException()
+        {
+            ITable table = new Table(5, 5);
+            IReport report = new Report();
+            IMove move = new Move();
+            IRobot robot = new Robot(table, move, report);
+
+            robot.PlaceRobot(3, 3, "NORTH");
+            Action result = () => robot.TurnRobot("");
+            Exception exception = Assert.Throws<Exception>(result);
+
+            Assert.Equal("Empty command", exception.Message);
+        }
+    }
+}
