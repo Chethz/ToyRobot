ecf907280809660257494e01644ffeab2e55243b
Cheth <chethiya.w@gmail.com>
2022-10-08 20:07:28 +1100

    Added Move class and tests.

---
+++ ToyRobot/Move.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToyRobot.Enums;
+using ToyRobot.Interfaces;
+
+namespace ToyRobot
+{
+    public class Move : IMove
+    {
+        public int NextX { get; set; }
+        public int NextY { get; set; }
+
+        public void NextLocation(int currentX, int currentY, Face face)
+        {
+            this.NextX = currentX;
+            this.NextY = currentY;
+
+            switch (face)
+            {
+                case Face.NORTH:
+                    NextY += 1;
+                    break;
+                case Face.SOUTH:
+                    NextY -= 1;
+                    break;
+                case Face.EAST:
+                    NextX += 1;
+                    break;
+                case Face.WEST:
+                    NextX -= 1;
+                    break;
+                default:
+                    throw new Exception();
+            }
+        }
+    }
+}
--- ToyRobotTests/MoveTests.cs
+++ ToyRobotTests/MoveTests.cs
@@ -1,10 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ToyRobot;
+using ToyRobot.Enums;
+using Xunit;
 
 namespace ToyRobotTests
 {
-    internal class MoveTests
+    public class MoveTests
     {
+        [Fact]
+        public void NextLocation_WhenFaceNorth_NextYSouldBeThree()
+        {
+            Move move = new Move();
+            move.NextX = 2;
+            move.NextY = 2;
+
+            move.NextLocation(2, 2, Face.NORTH);
+
+            Assert.True(move.NextY == 3);
+        }
+
+        [Fact]
+        public void NextLocation_WhenFaceEast_NextXSouldBeThree()
+        {
+            Move move = new Move();
+            move.NextX = 2;
+            move.NextY = 2;
+
+            move.NextLocation(2, 2, Face.EAST);
+
+            Assert.True(move.NextX == 3);
+        }
+
+        [Fact]
+        public void NextLocation_WhenFaceSouth_NextYSouldBeOne()
+        {
+            Move move = new Move();
+            move.NextX = 2;
+            move.NextY = 2;
+
+            move.NextLocation(2, 2, Face.SOUTH);
+
+            Assert.True(move.NextY == 1);
+        }
+
+        [Fact]
+        public void NextLocation_WhenFaceWest_NextXSouldBeOne()
+        {
+            Move move = new Move();
+            move.NextX = 2;
+            move.NextY = 2;
+
+            move.NextLocation(2, 2, Face.WEST);
+
+            Assert.True(move.NextX == 1);
+        }
     }
 }
